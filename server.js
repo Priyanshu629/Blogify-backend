bfbfe105ccb8bb3adabc707496ed2fbcc5a5c64c
priyanshu kumar mishra <mishrapriyanshu793@gmail.com>
2024-07-06 10:09:28 +0530

    fix backend route

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.use("/api/v1/check",isAuthenticated,(req,res)=>{
+app.get("/api/v1/check",isAuthenticated,(req,res)=>{
   return res.status(200).json({userId:req.user.userId})
 })
 app.use('/api/v1/user',userRouter)
