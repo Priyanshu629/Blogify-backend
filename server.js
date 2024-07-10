cf2583f233c74ce06bcd3eaddb9f3d6413215b00
priyanshu kumar mishra <mishrapriyanshu793@gmail.com>
2024-07-10 16:33:02 +0530

    done some changes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/v1/check", isAuthenticated, (req, res) => {
-  return res.status(200).json({ userId: req.user.userId });
+  return res.status(200).json({ userId: req.user.userId,username:req.user.username });
 });
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/blog", blogRouter);
